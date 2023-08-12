e28d3117cbc17604ca45de957c720d17c43d7def
DiogoS77 <diogo.messi.18@hotmail.com>
2023-08-12 02:43:09 +0100

    renamed Assignment to Reaction.js and adjusted it

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const {Schema, Types} = mongoose;
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: [true, "Reaction body is required."],
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required."],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
