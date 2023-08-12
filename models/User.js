e472f8dd13e5bf6685a81ea4b2b357ae695ffa46
DiogoS77 <diogo.messi.18@hotmail.com>
2023-08-12 01:43:18 +0100

    renamed student to User.js and adjusted it

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const {Schema, model} = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (value) => /.+@.+\..+/.test(value),
+        message: "Must match an email address!",
+      },
+    },
+    thoughts: [{type: Schema.Types.ObjectId, ref: "Thought"}],
+    friends: [{type: Schema.Types.ObjectId, ref: "User"}],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
