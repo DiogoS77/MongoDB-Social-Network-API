af01f748e165cdf56ffb5c9b2faa74619ba22106
DiogoS77 <diogo.messi.18@hotmail.com>
2023-08-13 21:34:20 +0100

    adjusted data.js by adding timeformat, date format

--- utils/data.js
+++ utils/data.js
@@ -120,7 +120,29 @@ function getRandomReactions() {
   return results;
 }
 
+function convertTimestampToFormattedString(timestamp) {
+  const date = new Date(timestamp);
+
+  const dateFormatConfig = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const timeFormatConfig = {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+
+  const formattedDate = date.toLocaleDateString("en-US", dateFormatConfig);
+  const formattedTime = date.toLocaleTimeString("en-US", timeFormatConfig);
+
+  return `${formattedDate} at ${formattedTime}`;
+}
+
 module.exports = {
   getRandomFullName,
   getRandomActivities,
+  convertTimestampToFormattedString,
 };
