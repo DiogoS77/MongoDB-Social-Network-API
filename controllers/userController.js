3f07ad2d31ba4301898d78b6eaa82950ded88d8f
DiogoS77 <diogo.messi.18@hotmail.com>
2023-08-14 00:37:11 +0100

    added update user to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,6 +65,24 @@ const userController = {
     }
   },
 
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        {_id: req.params.userId},
+        {$set: req.body},
+        {runValidators: true, new: true}
+      );
+
+      if (!user) {
+        res.status(404).json({message: "No user with this id!"});
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
   async removeFriend(req, res) {
     try {
       const user = await User.findByIdAndUpdate(
